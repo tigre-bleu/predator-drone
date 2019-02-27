605ea149215e1e4c0762d458395bbd2a8cc86a35
Léarch <learch@firost.fr>
2019-02-27 14:54:57 +0100

    Added root checker + quicker argument parser

--- main.py
+++ main.py
@@ -5,11 +5,24 @@
 
 from argparse import ArgumentParser
 from predator_drone import *
+import sys, os
 
 
 # ======================
 #    Useful functions
 # ======================
+
+def usage():
+    print("Usage:", sys.argv[0], " [-v <verb-level>] iface mon_iface")
+    print("  iface        will be the wireless interface used to connect to Parrot AR.Drone")
+    print("  mon_iface    will be the monitoring wireless interface")
+    print("  <verb-level> between", disp.Verb.ERROR, "and", disp.Verb.DEBUG)
+    print("     ", disp.Verb.ERROR,   "= ERROR")
+    print("     ", disp.Verb.WARNING, "= WARNING")
+    print("     ", disp.Verb.INFO,    "= INFO")
+    print("     ", disp.Verb.DEBUG,   "= DEBUG")
+    sys.exit(0)
+
 
 def clear_lists(menu, parrot_hacker):
     """ Clears all lists (registered APs, etc.). """
@@ -28,23 +41,44 @@ def clear_lists(menu, parrot_hacker):
 
 if __name__ == "__main__":
 
+    # Check root access
+    if os.geteuid() != 0:
+        disp.die("Please run this script as root!")
+
+
     # Parse arguments
-    parser = ArgumentParser(description="Drone predator")
-    parser.add_argument("--verb", "-v", type=int, default=disp.Verb.INFO,
-            help="script verbosity (0,.. => ERROR, WARNING, INFO, DEBUG)")
-    parser.add_argument("iface", type=str, default="",
-            help="wireless interface to be used to connect to Parrot")
-    parser.add_argument("mon_iface", type=str, default="",
-            help="wireless interface to be used in monitoring mode")
-    args = parser.parse_args()
+    iface     = ""
+    mon_iface = ""
+    argc = len(sys.argv)
+
+    try:
+        if argc == 3:
+            iface     = sys.argv[1]
+            mon_iface = sys.argv[2]
+
+        elif argc == 5 and sys.argv[1] == "-v":
+            verb_lvl = int(sys.argv[2])
+            if disp.Verb.ERROR <= verb_lvl <= disp.Verb.DEBUG:
+                disp.Verb.curr = verb_lvl
+                iface          = sys.argv[3]
+                mon_iface      = sys.argv[4]
+            else:
+                raise Exception
+        else:
+            raise Exception
+    except:
+        usage()
 
 
     # Check that the two interfaces are differents
-    if args.iface == args.mon_iface:
+    if iface == mon_iface:
         disp.die("The monitoring interface and managed one must be different!")
 
-    # Register verbosity
-    disp.Verb.curr = args.verb
+
+    # Hacking tools
+    wifi_mon = WifiManager(mon_iface, mon_mode=True)
+    wifi     = WifiManager(iface,     mon_mode=False)
+    parrot_list = ParrotAPsList(wifi, wifi_mon)
 
 
     # Display banner
@@ -55,12 +89,6 @@ if __name__ == "__main__":
             "   / /_/ / /  / /_/ / / / /  __/  / /_/ / /  /  __/ /_/ / /_/ / /_/ /_/ / /    \n"
             "  /_____/_/   \____/_/ /_/\___/  / .___/_/   \___/\__,_/\__,_/\__/\____/_/     \n"
             "                                /_/                                            \n")
-
-
-    # Hacking tools
-    wifi_mon = WifiManager(args.mon_iface, mon_mode=True)
-    wifi     = WifiManager(args.iface,     mon_mode=False)
-    parrot_list = ParrotAPsList(wifi, wifi_mon)
 
 
     # Main menu
