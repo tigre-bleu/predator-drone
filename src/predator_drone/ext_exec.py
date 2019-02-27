049088afe342b10fcc8085d1cb6470ebf27fea08
Léarch <learch@firost.fr>
2019-02-27 22:12:41 +0100

    Option get_output pour fonction do

--- src/predator_drone/ext_exec.py
+++ src/predator_drone/ext_exec.py
@@ -7,16 +7,22 @@ from . import disp
 import sys
 
 
-def do(*args, force_output=False):
+def do(*args, force_output=False, get_output=False):
     """ Execute an external command. """
     command     = ' '.join(args)
     show_output = disp.Verb.isdebug() or force_output
 
     try:
         disp.debug("Running '" + command + "'")
-        p = sbp.Popen(command, shell=True, stderr=sys.stderr,
-                stdout=(sys.stdout if show_output else sbp.DEVNULL))
-        p.wait()
+        if not get_output:
+            p = sbp.Popen(command, shell=True, stderr=sys.stderr,
+                    stdout=(sys.stdout if show_output else sbp.DEVNULL))
+            p.wait()
+        else:
+            p = sbp.Popen(command, shell=True, stderr=sbp.STDOUT, stdout=sbp.PIPE)
+            (output, _) = p.communicate()
+            p.wait()
+            return output
 
         if p.returncode < 0:
             disp.die("Command killed by signal", p.returncode)
